ed3c8655b5d38fd30908349527ee40752cb2d2ba
qixing <80924366+qixings@users.noreply.github.com>
2024-06-29 12:00:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,30 +6,3 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
 document.querySelector('.close-menu').addEventListener('click', function() {
     document.querySelector('.hamburger-menu-overlay').style.display = 'none';
 });
-
-// Image slider functionality
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slider img');
-const totalSlides = slides.length;
-const slideWidth = slides[0].clientWidth;
-const slider = document.querySelector('.slider');
-
-function showSlide(index) {
-    slider.style.transition = 'transform 0.5s ease'; // Smooth transition between slides
-    slider.style.transform = `translateX(-${index * slideWidth}px)`;
-}
-
-function nextSlide() {
-    if (currentSlide < totalSlides - 1) {
-        currentSlide++;
-    } else {
-        currentSlide = 0; // Reset to the first slide after the last slide
-    }
-    showSlide(currentSlide);
-}
-
-function startSlider() {
-    setInterval(nextSlide, 7000); // Change image every 3 seconds
-}
-
-document.addEventListener('DOMContentLoaded', startSlider);
